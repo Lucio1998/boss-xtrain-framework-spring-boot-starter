d720f8643e150e33e89455290a0c1db25eb6316c
Lucio1998 <2196990275@qq.com>
2019-12-05 23:02:35 +0800

    update annotation

---
+++ boss-xtrain-core/boss-xtrain-core-annotation/src/main/java/com/boss/xtrain/core/annotation/stuffer/DefaultEntityFieldStuffer.java
@@ -0,0 +1,9 @@
+package com.boss.xtrain.core.annotation.stuffer;
+
+/**
+ * @description：TODO
+ * @author： liuxin
+ * @create： 2019/12/5 下午 04:22
+ */
+public class DefaultEntityFieldStuffer implements EntityFieldStuffer {
+}
---
+++ boss-xtrain-core/boss-xtrain-core-annotation/src/main/java/com/boss/xtrain/core/annotation/stuffer/EntityFieldStuffer.java
@@ -0,0 +1,9 @@
+package com.boss.xtrain.core.annotation.stuffer;
+
+/**
+ * @description：对表的公用字段的持久化通过切面进行统一处理。
+ * @author： liuxin
+ * @create： 2019/12/5 下午 04:20
+ */
+public interface EntityFieldStuffer {
+}
